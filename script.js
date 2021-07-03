ed8d3ca88f4c98959f721dfc251338d31115b27f
Abdufattakh <84384279+Abdufattakh@users.noreply.github.com>
2021-07-03 22:20:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+// alert('калкуляторб сним вы можете решить примеры')
+
+// var a=prompt('Write first number'),
+//  b=prompt('write second number');
+//  var c=document.getElementById(elem);
+
+
+
+//   elem.onclick = function() {
+//     alert(Math.round(Math.sqrt(a*b))+' '+' answer');
+//   };
+
+const Textareas=document.querySelector('.name');
+
+Textareas.addEventListener('mousedown', function(){
+    Textareas.style.background='yellow';
+    Textareas.style.color='black';
+})
+Textareas.addEventListener('mouseout', function(){
+    Textareas.style.background='red';
+    Textareas.style.color='white';
+})
+
+const Textare=document.querySelector('.email');
+
+
+Textare.addEventListener('mousedown', function(){
+    Textare.style.background='yellow';
+    Textare.style.color='black';
+})
+Textare.addEventListener('mouseout', function(){
+    Textare.style.background='red';
+    Textare.style.color='white';
+})
+
+const Textar=document.querySelector('.num');
+
+Textar.addEventListener('mousedown', function(){
+    Textar.style.background='yellow';
+    Textar.style.color='black';
+})
+Textar.addEventListener('mouseout', function(){
+    Textar.style.background='red';
+    Textar.style.color='white';
+})
+
+
+
+const adde=document.querySelector('.name2');
+
+
+adde.addEventListener('mousedown', function(){
+    adde.style.background='yellow';
+    adde.style.color='black';
+})
+
+adde.addEventListener('mouseout', function(){
+    adde.style.background='red';
+    adde.style.color='white';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
